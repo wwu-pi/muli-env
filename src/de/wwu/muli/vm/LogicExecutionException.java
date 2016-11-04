0c85b11c5921d49fcd5bb40b4b2407c26ced2dcb
Jan Dageförde <jan.dagefoerde@ercis.uni-muenster.de>
2016-11-04 11:12:18 +0100

    (WIP) Logic VM does NOT inherit from SJVM anymore
    too much useless stuff...

---
+++ src/de/wwu/muli/vm/LogicExecutionException.java
@@ -0,0 +1,55 @@
+package de.wwu.muli.vm;
+
+import de.wwu.muggl.vm.execution.ExecutionException;
+
+/**
+ * Exception that is to be thrown on problems while Executing java bytecode in the symbolic execution mode.
+ * It indicates unexpected circumstances that lead to abnormal execution conditions.
+ *
+ * @author Tim Majchrzak
+ * @version 1.0.0, 2009-03-26
+ */
+public class LogicExecutionException extends ExecutionException {
+
+	/**
+	 * Constructs a new exception with null as its detail message.
+	 *
+	 * @see Exception#Exception()
+	 */
+	public LogicExecutionException() {
+		super();
+	}
+
+	/**
+	 * Constructs a new exception with the specified detail message.
+	 *
+	 * @param arg0 the detail message.
+	 * @see Exception#Exception(String)
+	 */
+	public LogicExecutionException(String arg0) {
+		super(arg0);
+	}
+
+	/**
+	 * Constructs a new exception with the specified detail message and cause.
+	 *
+	 * @param arg0 the detail message.
+	 * @param arg1 the cause.
+	 * @see Exception#Exception(String, Throwable)
+	 */
+	public LogicExecutionException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+	}
+
+	/**
+	 * Constructs a new exception with the specified cause and a detail message of (cause==null ?
+	 * null : cause.toString()).
+	 *
+	 * @param arg0 the cause.
+	 * @see Exception#Exception(Throwable)
+	 */
+	public LogicExecutionException(Throwable arg0) {
+		super(arg0);
+	}
+
+}

5a3dcac894b0bb1948bedf0a8201ab53b6252e79
Jan Dageförde <jan.dagefoerde@ercis.uni-muenster.de>
2016-11-11 17:22:45 +0100

    (WIP) custom search algo impls

---
+++ src/de/wwu/muli/env/search/LogicSearchAlgorithm.java
@@ -0,0 +1,56 @@
+package de.wwu.muli.env.search;
+
+import de.wwu.muggl.instructions.bytecode.LCmp;
+import de.wwu.muggl.instructions.general.CompareFp;
+import de.wwu.muggl.instructions.general.GeneralInstructionWithOtherBytes;
+import de.wwu.muggl.instructions.general.Switch;
+import de.wwu.muggl.search.SearchAlgorithm;
+import de.wwu.muggl.solvers.expressions.ConstraintExpression;
+import de.wwu.muggl.solvers.expressions.IntConstant;
+import de.wwu.muggl.solvers.expressions.Term;
+import de.wwu.muggl.symbolic.generating.Generator;
+import de.wwu.muggl.symbolic.searchAlgorithms.choice.ChoicePoint;
+import de.wwu.muggl.symbolic.searchAlgorithms.depthFirst.trailelements.ArrayRestore;
+import de.wwu.muggl.symbolic.searchAlgorithms.depthFirst.trailelements.FieldPut;
+import de.wwu.muggl.symbolic.searchAlgorithms.depthFirst.trailelements.Restore;
+import de.wwu.muggl.vm.execution.ConversionException;
+import de.wwu.muli.vm.LogicExecutionException;
+import de.wwu.muli.vm.LogicVirtualMachine;
+
+public interface LogicSearchAlgorithm extends SearchAlgorithm {
+	ChoicePoint getCurrentChoicePoint();
+
+	boolean trackBack(LogicVirtualMachine arg0);
+
+	void recoverState(LogicVirtualMachine arg0);
+
+	void generateNewChoicePoint(LogicVirtualMachine arg0, int arg1, Generator arg2)
+			throws ConversionException, LogicExecutionException;
+
+	void generateNewChoicePoint(LogicVirtualMachine arg0, String arg1) throws LogicExecutionException;
+
+	void generateNewChoicePoint(LogicVirtualMachine arg0, GeneralInstructionWithOtherBytes arg1,
+			ConstraintExpression arg2);
+
+	void generateNewChoicePoint(LogicVirtualMachine arg0, LCmp arg1, Term arg2, Term arg3)
+			throws LogicExecutionException;
+
+	void generateNewChoicePoint(LogicVirtualMachine arg0, CompareFp arg1, boolean arg2, Term arg3, Term arg4)
+			throws LogicExecutionException;
+
+	void generateNewChoicePoint(LogicVirtualMachine arg0, Switch arg1, Term arg2, IntConstant[] arg3, int[] arg4,
+			IntConstant arg5, IntConstant arg6) throws LogicExecutionException;
+
+	boolean savingFieldValues();
+
+	void saveFieldValue(FieldPut arg0);
+
+	boolean savingLocalVariableValues();
+
+	void saveLocalVariableValue(Restore arg0);
+
+	boolean savingArrayValues();
+
+	void saveArrayValue(ArrayRestore arg0);
+
+}
---
+++ src/de/wwu/muli/env/search/dfs/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Provides the depth first search algorithm implementation.
+ *
+ * @author Tim Majchrzak
+ */
+package de.wwu.muli.env.search.dfs;

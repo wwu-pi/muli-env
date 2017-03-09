b982be251805fa4c3a31ef96f0210a6698a69463
Jan Dageförde <jan.dagefoerde@ercis.uni-muenster.de>
2017-03-09 17:43:19 +0100

    configure loggers

--- src/de/wwu/muli/env/MuliRunner.java
+++ src/de/wwu/muli/env/MuliRunner.java
@@ -145,8 +145,8 @@ public class MuliRunner {
 		Options.getInst().actualCliPrinting = true;
 		
 		Globals.getInst().changeLogLevel(Level.TRACE);
-		Globals.getInst().execLogger.addAppender(new ConsoleAppender(new SimpleLayout(), "System.err"));
-		//Globals.getInst().logger.addAppender(new ConsoleAppender(new SimpleLayout()));
+		Globals.getInst().parserLogger.setLevel(Level.INFO);
+
 		
 		// Accept class
 		final String className = args[0];

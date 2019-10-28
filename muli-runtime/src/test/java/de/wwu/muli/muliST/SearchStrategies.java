b1cead6f734baf346e44ca2e08e011121f7f21d2
Jan Dageförde <jan.dagefoerde@ercis.uni-muenster.de>
2019-10-28 17:08:38 +0100

    Add a test that has different output for BFS/DFS

--- muli-runtime/src/test/java/de/wwu/muli/muliST/SearchStrategies.java
+++ muli-runtime/src/test/java/de/wwu/muli/muliST/SearchStrategies.java
@@ -12,16 +12,66 @@ import static org.junit.Assert.assertEquals;
 
 public class SearchStrategies {
     @Test
-    public final void test_BreadthFirstSearch() throws InterruptedException, ClassFileException {
+    public final void test_BreadthFirstSearchF2C() throws InterruptedException, ClassFileException {
+        ST[] foundTrees = TestableMuliRunner.runApplication("applications.muliST.FlipTwoCoinsBFS");
+        assertEquals(1, foundTrees.length);
+        //System.out.println(foundTrees[0].toString());
+        /* Expected tree is
+            - Choice
+                - Choice
+                    - Value Objectref java.lang.Boolean (id: 10410 val: true)
+                    - Fail
+                - Value Objectref java.lang.Boolean (id: 10438 val: false)
+         */
+        Object[] solutions = LazyDFSIterator.stream(foundTrees[0]).toArray();
+        assertEquals(2, solutions.length);
+        assertEquals("false\ntrue\n", TestablePrintStreamWrapper.outputStream().getBufferContents());
+    }
+
+    @Test
+    public final void test_DepthFirstSearchF2C() throws InterruptedException, ClassFileException {
+        ST[] foundTrees = TestableMuliRunner.runApplication("applications.muliST.FlipTwoCoinsDFS");
+        assertEquals(1, foundTrees.length);
+        Object[] solutions = LazyDFSIterator.stream(foundTrees[0]).toArray();
+        assertEquals(2, solutions.length);
+        assertEquals("true\nfalse\n", TestablePrintStreamWrapper.outputStream().getBufferContents());
+    }
+
+    @Test
+    public final void test_IterativeDeepeningSearchF2C() throws InterruptedException, ClassFileException {
+        ST[] foundTrees = TestableMuliRunner.runApplication("applications.muliST.FlipTwoCoinsIDD");
+        assertEquals(1, foundTrees.length);
+        Object[] solutions = LazyDFSIterator.stream(foundTrees[0]).toArray();
+        assertEquals(2, solutions.length);
+        assertEquals("true\nfalse\n", TestablePrintStreamWrapper.outputStream().getBufferContents());
+    }
+    @Test
+    public final void test_BreadthFirstSearchCC() throws InterruptedException, ClassFileException {
         ST[] foundTrees = TestableMuliRunner.runApplication("applications.muliST.ComplicatedCoinsBFS");
         assertEquals(1, foundTrees.length);
+        //System.out.println(foundTrees[0].toString());
+        /* Expected tree is
+            - Choice
+                - Choice
+                    - Choice
+                        - Value Objectref java.lang.Boolean (id: 10414 val: false)
+                        - Fail
+                    - Choice
+                        - Value Objectref java.lang.Boolean (id: 10441 val: false)
+                        - Fail
+                - Choice
+                    - Value Objectref java.lang.Boolean (id: 10444 val: false)
+                    - Choice
+                        - Value Objectref java.lang.Boolean (id: 10447 val: true)
+                        - Fail
+         */
         Object[] solutions = LazyDFSIterator.stream(foundTrees[0]).toArray();
         assertEquals(4, solutions.length);
         assertEquals("false\nfalse\nfalse\ntrue\n", TestablePrintStreamWrapper.outputStream().getBufferContents());
     }
 
     @Test
-    public final void test_DepthFirstSearch() throws InterruptedException, ClassFileException {
+    public final void test_DepthFirstSearchCC() throws InterruptedException, ClassFileException {
         ST[] foundTrees = TestableMuliRunner.runApplication("applications.muliST.ComplicatedCoinsDFS");
         assertEquals(1, foundTrees.length);
         Object[] solutions = LazyDFSIterator.stream(foundTrees[0]).toArray();
@@ -30,10 +80,9 @@ public class SearchStrategies {
     }
 
     @Test
-    public final void test_IterativeDeepeningSearch() throws InterruptedException, ClassFileException {
+    public final void test_IterativeDeepeningSearchCC() throws InterruptedException, ClassFileException {
         ST[] foundTrees = TestableMuliRunner.runApplication("applications.muliST.ComplicatedCoinsIDD");
         assertEquals(1, foundTrees.length);
-        System.out.println(foundTrees[0].toString());
         Object[] solutions = LazyDFSIterator.stream(foundTrees[0]).toArray();
         assertEquals(4, solutions.length);
         assertEquals("false\nfalse\nfalse\ntrue\n", TestablePrintStreamWrapper.outputStream().getBufferContents());

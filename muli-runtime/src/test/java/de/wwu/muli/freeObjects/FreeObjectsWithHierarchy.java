78256b54f07a02a1468d89ab6f62005f5e89539c
Jan Dageförde <jan.dagefoerde@ercis.uni-muenster.de>
2019-12-19 14:03:08 +0100

    (correctly failing) enable test case for abstract methods on free objects
    expect: nd-branching over all applicable implementations

--- muli-runtime/src/test/java/de/wwu/muli/freeObjects/FreeObjectsWithHierarchy.java
+++ muli-runtime/src/test/java/de/wwu/muli/freeObjects/FreeObjectsWithHierarchy.java
@@ -13,7 +13,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class FreeObjectsWithHierarchy {
-    // Disabled @Test
+    @Test
     public final void test_methodInSubclasses() throws InterruptedException, ClassFileException {
         ST[] foundTrees = TestableMuliRunner.runApplication("applications.freeObjects.InheritedMethod");
         assertEquals(1, foundTrees.length);
@@ -21,6 +21,6 @@ public class FreeObjectsWithHierarchy {
         Object[] leaves = LazyDFSIterator.stream(foundTrees[0]).toArray();
         // One for Rectangle, one for Square, one failing.
         assertEquals(3, leaves.length);
-        assertEquals(1, Arrays.stream(leaves).filter(x -> x instanceof Value).count());
+        assertEquals(2, Arrays.stream(leaves).filter(x -> x instanceof Value).count());
     }
 }

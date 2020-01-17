52ea6cb828e345b5b66bd55a157acf79ef90ec5c
Jan Dageförde <jan.dagefoerde@ercis.uni-muenster.de>
2020-01-17 15:52:39 +0100

    Add test case variation for invokevirtual instead of invokeinterface

--- muli-runtime/src/test/java/de/wwu/muli/freeObjects/FreeObjectsWithHierarchy.java
+++ muli-runtime/src/test/java/de/wwu/muli/freeObjects/FreeObjectsWithHierarchy.java
@@ -16,7 +16,7 @@ import static org.junit.Assert.assertTrue;
 
 public class FreeObjectsWithHierarchy {
     @Test
-    public final void test_methodInSubclasses() throws InterruptedException, ClassFileException {
+    public final void test_methodImplementingInterface() throws InterruptedException, ClassFileException {
         ST[] foundTrees = TestableMuliRunner.runApplication("applications.freeObjects.InheritedMethod");
         assertEquals(1, foundTrees.length);
         System.out.println(foundTrees[0].toString());
@@ -28,5 +28,19 @@ public class FreeObjectsWithHierarchy {
         // Show that the free object was interpreted with the correct type.
         assertTrue(collect.get(0).toString().endsWith(" val: applications.freeObjects.pojo.Rectangle)"));
         assertTrue(collect.get(1).toString().endsWith(" val: applications.freeObjects.pojo.Square)"));
+    }
+    @Test
+    public final void test_methodImplementingAbstractClass() throws InterruptedException, ClassFileException {
+        ST[] foundTrees = TestableMuliRunner.runApplication("applications.freeObjects.InheritedAbstractMethod");
+        assertEquals(1, foundTrees.length);
+        System.out.println(foundTrees[0].toString());
+        Object[] leaves = LazyDFSIterator.stream(foundTrees[0]).toArray();
+        // A binary choice for getArea, then one binary choice each for true/false -> One for Rectangle, one for Square, two failing.
+        assertEquals(4, leaves.length);
+        assertEquals(2, Arrays.stream(leaves).filter(x -> x instanceof Value).count());
+        List<Object> collect = Arrays.stream(leaves).filter(x -> x instanceof Value).map(v -> ((Value) v).value).collect(Collectors.toList());
+        // Show that the free object was interpreted with the correct type.
+        assertTrue(collect.get(0).toString().endsWith(" val: applications.freeObjects.pojo2.Rectangle)"));
+        assertTrue(collect.get(1).toString().endsWith(" val: applications.freeObjects.pojo2.Square)"));
     }
 }

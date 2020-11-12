81b4ec46a8de64839d2c60e6c97c2eee914d96ac
Hendrik Winkelmann <hendrik.winkelmann@uni-muenster.de>
2020-11-12 11:07:40 +0100

    Simple tests for free arrays

---
+++ muli-runtime/src/test/java/de/wwu/muli/freeArrays/CheckPrimitiveFreeArray.java
@@ -0,0 +1,18 @@
+package de.wwu.muli.freeArrays;
+
+import de.wwu.muggl.vm.classfile.ClassFileException;
+import de.wwu.muli.env.TestableMuliRunner;
+import de.wwu.muli.searchtree.ST;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CheckPrimitiveFreeArray {
+
+    @Test
+    public final void test_checkPrimitiveFreeArray() throws InterruptedException, ClassFileException {
+        ST[] foundTrees = TestableMuliRunner.runApplication("applications.freeArrays.CheckPrimitiveFreeArray");
+        assertEquals(3, foundTrees.length);
+    }
+}

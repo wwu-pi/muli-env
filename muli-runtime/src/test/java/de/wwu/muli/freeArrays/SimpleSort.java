2a3f232efaf1d67fbb303dbfe35a32a7ffe02091
Hendrik Winkelmann <hendrik.winkelmann@uni-muenster.de>
2021-01-14 15:21:44 +0100

    Fixed reference value of FreeArrayref

--- muli-runtime/src/test/java/de/wwu/muli/freeArrays/SimpleSort.java
+++ muli-runtime/src/test/java/de/wwu/muli/freeArrays/SimpleSort.java
@@ -3,6 +3,7 @@ package de.wwu.muli.freeArrays;
 import de.wwu.muggl.configuration.Defaults;
 import de.wwu.muggl.solvers.expressions.IntConstant;
 import de.wwu.muggl.vm.classfile.ClassFileException;
+import de.wwu.muggl.vm.initialization.Arrayref;
 import de.wwu.muggl.vm.initialization.FreeArrayref;
 import de.wwu.muli.env.LazyDFSIterator;
 import de.wwu.muli.env.TestableMuliRunner;
@@ -30,18 +31,18 @@ public class SimpleSort {
         ST[] foundTrees = TestableMuliRunner.runApplication("applications.freeArrays.SimpleSort");
         Object[] leaves = LazyDFSIterator.stream(foundTrees[0]).toArray();
         assertEquals(1, leaves.length);
-        FreeArrayref sortedArray = (FreeArrayref) ((Value) leaves[0]).value;
-        assertEquals(sortedArray.getLength(), sortedArray.getFreeArrayElements().size());
-        assertEquals(IntConstant.getInstance(1),  sortedArray.getFreeArrayElement(0));
-        assertEquals(IntConstant.getInstance(5),  sortedArray.getFreeArrayElement(1));
-        assertEquals(IntConstant.getInstance(8),  sortedArray.getFreeArrayElement(2));
-        assertEquals(IntConstant.getInstance(12), sortedArray.getFreeArrayElement(3));
-        assertEquals(IntConstant.getInstance(17), sortedArray.getFreeArrayElement(4));
-        assertEquals(IntConstant.getInstance(27), sortedArray.getFreeArrayElement(5));
-        assertEquals(IntConstant.getInstance(39), sortedArray.getFreeArrayElement(6));
-        assertEquals(IntConstant.getInstance(42), sortedArray.getFreeArrayElement(7));
-        assertEquals(IntConstant.getInstance(56), sortedArray.getFreeArrayElement(8));
-        assertEquals(IntConstant.getInstance(78), sortedArray.getFreeArrayElement(9));
+        Arrayref sortedArray = (Arrayref) ((Value) leaves[0]).value;
+        assertEquals(10, sortedArray.getLength());
+        assertEquals(IntConstant.getInstance(1),  sortedArray.getElement(0));
+        assertEquals(IntConstant.getInstance(5),  sortedArray.getElement(1));
+        assertEquals(IntConstant.getInstance(8),  sortedArray.getElement(2));
+        assertEquals(IntConstant.getInstance(12), sortedArray.getElement(3));
+        assertEquals(IntConstant.getInstance(17), sortedArray.getElement(4));
+        assertEquals(IntConstant.getInstance(27), sortedArray.getElement(5));
+        assertEquals(IntConstant.getInstance(39), sortedArray.getElement(6));
+        assertEquals(IntConstant.getInstance(42), sortedArray.getElement(7));
+        assertEquals(IntConstant.getInstance(56), sortedArray.getElement(8));
+        assertEquals(IntConstant.getInstance(78), sortedArray.getElement(9));
         System.out.println(sortedArray);
     }
 

caeb6775e37f3c35e64aa6a5ff164089e353ebaf
Laur0r <laura-troost@t-online.de>
2021-02-23 11:00:55 +0100

    Distance algorithms

--- muli-runtime/src/test/java/de/wwu/muli/tcg/TspTest.java
+++ muli-runtime/src/test/java/de/wwu/muli/tcg/TspTest.java
@@ -4,22 +4,13 @@ import de.wwu.muggl.vm.classfile.ClassFileException;
 import de.wwu.muli.env.LazyDFSIterator;
 import de.wwu.muli.env.TestableMuliRunner;
 import de.wwu.muli.searchtree.ST;
-import de.wwu.muli.searchtree.Value;
 import org.junit.Test;
 
-import java.util.ArrayList;
-
-public class BellmanFordTest {
+public class TspTest {
 
     @Test // Not working
     public final void testVariables() throws InterruptedException, ClassFileException {
-        ST[] foundTrees = TestableMuliRunner.runApplication("applications.tcg.MainBellman");
+        ST[] foundTrees = TestableMuliRunner.runApplication("applications.tcg.MainTSP");
         Object[] leaves = LazyDFSIterator.stream(foundTrees[0]).toArray();
-        ArrayList<Object> solutions = new ArrayList<Object>();
-        for(Object leaf : leaves){
-            if(leaf instanceof Value){
-                solutions.add(leaf);
-            }
-        }
     }
 }

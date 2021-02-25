311ac233b5389ecd9c7ddba564a1d649db3dfed9
Laur0r <laura-troost@t-online.de>
2021-02-25 10:43:34 +0100

    Tcg Examples

---
+++ muli-runtime/src/test/java/de/wwu/muli/tcg/BellmanFordTest.java
@@ -0,0 +1,25 @@
+package de.wwu.muli.tcg;
+
+import de.wwu.muggl.vm.classfile.ClassFileException;
+import de.wwu.muli.env.LazyDFSIterator;
+import de.wwu.muli.env.TestableMuliRunner;
+import de.wwu.muli.searchtree.ST;
+import de.wwu.muli.searchtree.Value;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+public class BellmanFordTest {
+
+    @Test // Not working
+    public final void testVariables() throws InterruptedException, ClassFileException {
+        ST[] foundTrees = TestableMuliRunner.runApplication("applications.tcg.MainBellman");
+        Object[] leaves = LazyDFSIterator.stream(foundTrees[0]).toArray();
+        ArrayList<Object> solutions = new ArrayList<Object>();
+        for(Object leaf : leaves){
+            if(leaf instanceof Value){
+                solutions.add(leaf);
+            }
+        }
+    }
+}

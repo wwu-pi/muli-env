10059c84e2582f1af879e9d644bb6522d5c90c86
Hendrik Winkelmann <hendrik.winkelmann@uni-muenster.de>
2021-05-05 19:58:10 +0200

    Added working Logistics-Example with free objects

---
+++ muli-runtime/src/test/java/de/wwu/muli/freeObjectsUses/Logistics.java
@@ -0,0 +1,27 @@
+package de.wwu.muli.freeObjectsUses;
+
+import de.wwu.muggl.vm.classfile.ClassFileException;
+import de.wwu.muggl.vm.execution.nativeWrapping.TestablePrintStreamWrapper;
+import de.wwu.muli.env.LazyDFSIterator;
+import de.wwu.muli.env.TestableMuliRunner;
+import de.wwu.muli.searchtree.ST;
+import de.wwu.muli.searchtree.Value;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class Logistics {
+    @Test
+    public final void testLogistics() throws InterruptedException, ClassFileException {
+        ST[] foundTrees = TestableMuliRunner.runApplication("applications.freeObjectsUses.Logistics");
+        Object[] leaves = LazyDFSIterator.stream(foundTrees[0]).toArray();
+        int solutionCount = 0;
+        for (Object l : leaves) {
+            if (l instanceof Value) {
+                solutionCount++;
+            }
+        }
+        assertEquals(1, solutionCount);
+        TestablePrintStreamWrapper.outputStream().resetBuffer(); // To reset output stream
+    }
+}

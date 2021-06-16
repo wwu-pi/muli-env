007fa4a75d8c42b36872888b94bdaf0edd9c59b8
Laur0r <laura-troost@t-online.de>
2021-06-16 14:24:40 +0200

    new examples

---
+++ muli-runtime/src/test/java/de/wwu/muli/tcg/Hanoi.java
@@ -0,0 +1,16 @@
+package de.wwu.muli.tcg;
+
+import de.wwu.muggl.vm.classfile.ClassFileException;
+import de.wwu.muli.env.LazyDFSIterator;
+import de.wwu.muli.env.TestableMuliRunner;
+import de.wwu.muli.searchtree.ST;
+import org.junit.Test;
+
+public class Hanoi {
+
+    @Test
+    public final void testVariables() throws InterruptedException, ClassFileException {
+        ST[] foundTrees = TestableMuliRunner.runApplication("applications.tcg.MainHanoi");
+        Object[] leaves = LazyDFSIterator.stream(foundTrees[0]).toArray();
+    }
+}
